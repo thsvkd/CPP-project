1b191bdd69999ab794b5df3083d44a0d0d21df19
thsvkd <thsxogud1123@gmail.com>
2020-10-03 23:21:28 +0900

    make private branch

--- database_controll.h
+++ database_controll.h
@@ -1,6 +1,11 @@
 #pragma once
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "database_controll.h"
 #include "node_controll.h"
 
-char* ReadFile(char* file_name);
-void WriteFile(char* file_name);
+char *ReadFile(char *file_name);
+void WriteFile(char *file_name);
+void Make_Linkedlist(char* data);
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "node_controll.h"
+#include "database_controll.h"
 
 enum state
 {
@@ -17,10 +18,18 @@ int main()
 	//tail = CreateNode((char*)"tail", 0);
 	head->nextNode = tail;
 
-	InsertNode(head, (char *)"thsvkd1", 56);
+	char *tmp = ReadFile((char*)"database.txt");
+	printf("read data : \n%s", tmp);
+
+	Make_Linkedlist(tmp);
+
+	/*InsertNode(head, (char *)"thsvkd1", 33);
 	InsertNode(head, (char *)"thsvkd2", 76);
 	InsertNode(head, (char *)"thsvkd3", 34);
 	InsertNode(head, (char *)"thsvkd4", 12);
+	InsertNode(head, (char *)"thsvkd5", 112);*/
+
+	WriteFile((char*)"database.txt");
 
 	PrintNode();
 	Node *found_node = FindNode((char *)"thsvkd2");
